9074deb05f5fa4b347525546461a328d23abb0aa
chabaug <augustolnasso@gmail.com>
2015-04-13 08:03:04 -0300

    lunes mañana

--- altaLista_asm.asm
+++ altaLista_asm.asm
@@ -24,22 +24,22 @@
 
 ; /** DEFINES **/    >> SE RECOMIENDA COMPLETAR LOS DEFINES CON LOS VALORES CORRECTOS
 	%define NULL 	0
-	%define TRUE 	0
+	%define TRUE 	1
 	%define FALSE 	0
 
-	%define ALTALISTA_SIZE     		0
+	%define ALTALISTA_SIZE     		16
 	%define OFFSET_PRIMERO 			0
-	%define OFFSET_ULTIMO  			0
+	%define OFFSET_ULTIMO  			8
 
-	%define NODO_SIZE     			0
+	%define NODO_SIZE     			24
 	%define OFFSET_SIGUIENTE   		0
-	%define OFFSET_ANTERIOR   		0
-	%define OFFSET_DATO 			0
+	%define OFFSET_ANTERIOR   		8
+	%define OFFSET_DATO 			16
 
-	%define ESTUDIANTE_SIZE  		0
+	%define ESTUDIANTE_SIZE  		20
 	%define OFFSET_NOMBRE 			0
-	%define OFFSET_GRUPO  			0
-	%define OFFSET_EDAD 			0
+	%define OFFSET_GRUPO  			8
+	%define OFFSET_EDAD 			16
 
 
 section .rodata
@@ -55,7 +55,23 @@ section .text
 
 	; estudiante *estudianteCrear( char *nombre, char *grupo, unsigned int edad );
 	estudianteCrear:
-		; COMPLETAR AQUI EL CODIGO
+		push rbp
+		mov rbp, rsp
+		push rbx
+		push r12
+		push r13
+		push r14
+		push r15
+
+
+
+		pop r15
+		pop r14
+		pop r13
+		pop r12
+		pop rbx
+		pop rbp
+		ret
 
 	; void estudianteBorrar( estudiante *e );
 	estudianteBorrar:
@@ -113,3 +129,28 @@ section .text
 	filtrarAltaLista:
 		; COMPLETAR AQUI EL CODIGO
 
+;/** FUNCIONES AUXILIARES **/
+;----------------------------
+
+	; unsigned char string_longitud( char *s )
+	string_longitud:
+		; el puntero a char esta en rdi
+		push rbp
+		mov rbp, rsp
+
+		mov rax, 0 	; inicializo el contador
+
+		string_longitud.sumar:		; ciclo que suma direccion a direccion
+			cmp BYTE [rdi+rax], 0		; chequea si el byte de la direccion del string es cero
+			jz string_longitud.terminar	; if [rdi+rax] == 0 => terminar
+			inc rax				; else rax++
+			jmp string_longitud.sumar	; repite
+
+		string_longitud.terminar:	; cuando termina el ciclo
+			pop rbp
+			ret
+	
+	; en rax tengo la longitud del string
+
+	
+		
